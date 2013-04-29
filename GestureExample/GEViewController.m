992bba34200eba5d829dea3b0150003e804534a2
Pramod Kumar Pranav <pkpranav2011@gmail.com>
2013-04-29 17:06:01 +0530

    Second Commit

--- GestureExample/GEViewController.m
+++ GestureExample/GEViewController.m
@@ -13,17 +13,80 @@
 @end
 
 @implementation GEViewController
+@synthesize imageView;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    self.imageView.userInteractionEnabled = YES;
+    
+    UIPanGestureRecognizer *panRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panDetected:)];
+    [self.imageView addGestureRecognizer:panRecognizer];
+    
+    UIPinchGestureRecognizer *pinchRecognizer = [[UIPinchGestureRecognizer alloc] initWithTarget:self action:@selector(pinchDetected:)];
+    [self.imageView addGestureRecognizer:pinchRecognizer];
+    
+    UIRotationGestureRecognizer *rotationRecognizer = [[UIRotationGestureRecognizer alloc] initWithTarget:self action:@selector(rotationDetected:)];
+    [self.imageView addGestureRecognizer:rotationRecognizer];
+    
+    UITapGestureRecognizer *tapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapDetected:)];
+    tapRecognizer.numberOfTapsRequired = 2;
+    [self.imageView addGestureRecognizer:tapRecognizer];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)dealloc {
+    [imageView release];
+    [super dealloc];
+}
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+        return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+    } else {
+        return YES;
+    }
+}
+
+#pragma mark - Gesture Recognizers
+
+- (void)panDetected:(UIPanGestureRecognizer *)panRecognizer
+{
+    CGPoint translation = [panRecognizer translationInView:self.view];
+    CGPoint imageViewPosition = self.imageView.center;
+    imageViewPosition.x += translation.x;
+    imageViewPosition.y += translation.y;
+    
+    self.imageView.center = imageViewPosition;
+    [panRecognizer setTranslation:CGPointZero inView:self.view];
+}
+
+- (void)pinchDetected:(UIPinchGestureRecognizer *)pinchRecognizer
+{
+    CGFloat scale = pinchRecognizer.scale;
+    self.imageView.transform = CGAffineTransformScale(self.imageView.transform, scale, scale);
+    pinchRecognizer.scale = 1.0;
+}
+
+- (void)rotationDetected:(UIRotationGestureRecognizer *)rotationRecognizer
+{
+    CGFloat angle = rotationRecognizer.rotation;
+    self.imageView.transform = CGAffineTransformRotate(self.imageView.transform, angle);
+    rotationRecognizer.rotation = 0.0;
+}
+
+- (void)tapDetected:(UITapGestureRecognizer *)tapRecognizer
+{
+    [UIView animateWithDuration:0.25 animations:^{
+        self.imageView.center = CGPointMake(CGRectGetMidX(self.view.bounds), CGRectGetMidY(self.view.bounds));
+        self.imageView.transform = CGAffineTransformIdentity;
+    }];
 }
 
 @end
